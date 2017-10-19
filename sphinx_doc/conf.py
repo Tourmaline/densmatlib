2232116bf31cfc5503349df853331fa43bbdcc5a
Tourmaline <nastja.kruchinina@gmail.com>
2017-10-19 15:29:37 +0200

    test

--- sphinx_doc/conf.py
+++ sphinx_doc/conf.py
@@ -20,14 +20,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('./densmatlib'))
-sys.path.insert(0, os.path.abspath('../densmatlib'))
-sys.path.insert(0, os.path.abspath('./densmatlib/densematrix'))
-sys.path.insert(0, os.path.abspath('../densmatlib/densematrix'))
-sys.path.insert(0, os.path.abspath('../../../densmatlib'))
-sys.path.insert(0, os.path.abspath('../../densmatlib'))
-sys.path.insert(0, os.path.abspath('../../../../densmatlib'))
 
 
 # -- General configuration ------------------------------------------------
