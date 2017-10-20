b8494c1fc60477a0b190ce897f673aac0d444464
Tourmaline <nastja.kruchinina@gmail.com>
2017-10-20 14:41:54 +0200

    xelatex -> lualatex

--- sphinx_doc/conf.py
+++ sphinx_doc/conf.py
@@ -131,7 +131,7 @@ htmlhelp_basename = 'Recursivedensitymatrixexpansiondoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_engine = 'xelatex'
+latex_engine = 'lualatex'
 
 latex_elements = {
     # #The paper size ('letterpaper' or 'a4paper')
