e210eaa3790a923991140c57b834b9ee8d59076b
Tourmaline <nastja.kruchinina@gmail.com>
2017-10-20 13:29:57 +0200

    Test

--- sphinx_doc/conf.py
+++ sphinx_doc/conf.py
@@ -38,8 +38,7 @@ sys.path.insert(0, os.path.abspath('../densmatlib/densematrix'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -133,21 +132,21 @@ htmlhelp_basename = 'Recursivedensitymatrixexpansiondoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-    #The paper size ('letterpaper' or 'a4paper')
-    'papersize': 'letterpaper',
-    #The font size ('10pt', '11pt' or '12pt').
-    'pointsize': '10pt',
-    #Additional stuff for the LaTeX preamble.
-    'preamble': '',
-    #Latex figure (float) alignment
-    'figure_align': 'htbp',
+    # #The paper size ('letterpaper' or 'a4paper')
+    # 'papersize': 'letterpaper',
+    # #The font size ('10pt', '11pt' or '12pt').
+    # 'pointsize': '10pt',
+    # #Additional stuff for the LaTeX preamble.
+    # 'preamble': '',
+    # #Latex figure (float) alignment
+    # 'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Recursivedensitymatrixexpansion.tex', 'densmatlib',
+    (master_doc, 'Recexp.tex', 'densmatlib',
      'Anastasia Kruchinina', 'manual'),
 ]
 
@@ -157,7 +156,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'recursivedensitymatrixexpansion', 'densmatlib Documentation',
+    (master_doc, 'recexp', 'densmatlib Documentation',
      [author], 1)
 ]
 
@@ -168,8 +167,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Recursivedensitymatrixexpansion', 'densmatlib Documentation',
-     author, 'Recursivedensitymatrixexpansion', 'Recursive density matrix expansion!!!',
+    (master_doc, 'Recexp', 'densmatlib Documentation',
+     author, 'Recexp', 'Recursive density matrix expansion',
      'Miscellaneous'),
 ]
 
